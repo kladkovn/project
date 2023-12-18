c1001686eb1c95ee76ea195fb0b85959a6e2b5ce
VOLODYMYR KLADKO <kladkovn@gmail.com>
2023-12-18 14:44:50 +0200

    'first step'

--- src/route/test.js
+++ src/route/test.js
@@ -21,7 +21,7 @@ router.get('/', function (req, res) {
     component: ['heading'],
 
     // вказуємо назву сторінки
-    title: 'Назва сторінки',
+    title: 'create options',
     // ... сюди можна далі продовжувати додавати потрібні технічні дані, які будуть використовуватися в layout
 
     // вказуємо дані,
@@ -32,5 +32,114 @@ router.get('/', function (req, res) {
   // ↑↑ сюди вводимо JSON дані
 })
 
+router.get('/create-account', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('create-account', {
+    // вказуємо назву контейнера
+    name: 'create-account',
+    // вказуємо назву компонентів
+    component: ['heading'],
+
+    // вказуємо назву сторінки
+    title: 'create account',
+    // ... сюди можна далі продовжувати додавати потрібні технічні дані, які будуть використовуватися в layout
+
+    // вказуємо дані,
+    data: {
+      
+    },
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+router.get('/create-login', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('create-login', {
+    // вказуємо назву контейнера
+    name: 'create-login',
+    // вказуємо назву компонентів
+    component: ['heading'],
+
+    // вказуємо назву сторінки
+    title: 'create login',
+    // ... сюди можна далі продовжувати додавати потрібні технічні дані, які будуть використовуватися в layout
+
+    // вказуємо дані,
+    data: {
+      
+    },
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+router.get('/recovery', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('recovery', {
+    // вказуємо назву контейнера
+    name: 'recovery',
+    // вказуємо назву компонентів
+    component: ['heading'],
+
+    // вказуємо назву сторінки
+    title: 'recovery',
+    // ... сюди можна далі продовжувати додавати потрібні технічні дані, які будуть використовуватися в layout
+
+    // вказуємо дані,
+    data: {
+      
+    },
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+router.get('/type-account', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('type-account', {
+    // вказуємо назву контейнера
+    name: 'type-account',
+    // вказуємо назву компонентів
+    component: ['heading'],
+
+    // вказуємо назву сторінки
+    title: 'type-account',
+    // ... сюди можна далі продовжувати додавати потрібні технічні дані, які будуть використовуватися в layout
+
+    // вказуємо дані,
+    data: {
+      
+    },
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+router.get('/contact-form', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('contact-form', {
+    // вказуємо назву контейнера
+    name: 'contact-form',
+    // вказуємо назву компонентів
+    component: ['heading'],
+
+    // вказуємо назву сторінки
+    title: 'contact-form',
+    // ... сюди можна далі продовжувати додавати потрібні технічні дані, які будуть використовуватися в layout
+
+    // вказуємо дані,
+    data: {
+      
+    },
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
 // Підключаємо роутер до бек-енду
 module.exports = router
--- src/script/test.js
+++ src/script/test.js
@@ -1,12 +1,3 @@
-// console.log('script')
+console.log('script')
 
 
-export const message = 'script';
-
-export const message2 =() => {}
-
-export function Message3() {}
-
-const message3 = ''
-
-export {message3, Message3, message2}
